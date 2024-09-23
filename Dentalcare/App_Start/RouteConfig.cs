ec81a4cccc27e5cf9c096389abfbb66f0f0a5572
DinhPhatPhat <dinhtainang@gmail.com>
2024-09-23 23:01:17 +0700

    Dentalcare 0.2 Thêm giao diện, cấu hình routes. Làm việc trên DefaultController, không làm việc trên HomeController

--- Dentalcare/App_Start/RouteConfig.cs
+++ Dentalcare/App_Start/RouteConfig.cs
@@ -12,12 +12,42 @@ namespace Dentalcare
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute(
+                name: "About",
+                url: "About",
+                defaults: new { controller = "Default", action = "About", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Service",
+                url: "Service",
+                defaults: new { controller = "Default", action = "Service", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Doctors",
+                url: "Doctors",
+                defaults: new { controller = "Default", action = "Doctors", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Blog",
+                url: "Blog",
+                defaults: new { controller = "Default", action = "Blog", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Contact",
+                url: "Contact",
+                defaults: new { controller = "Default", action = "Contact", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Login",
+                url: "Login",
+                defaults: new { controller = "Default", action = "Login", id = UrlParameter.Optional }
+            );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Default", action = "Home", id = UrlParameter.Optional }
             );
+
         }
     }
 }
--- Dentalcare/Controllers/DefaultController.cs
+++ Dentalcare/Controllers/DefaultController.cs
@@ -9,8 +9,46 @@ namespace Dentalcare.Controllers
     public class DefaultController : Controller
     {
 
-        public ActionResult Home() {
+        public ActionResult Home()
+        {
             return View();
         }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About.";
+            return View();
+
+        }
+
+        public ActionResult Service()
+        {
+            ViewBag.Message = "Sevice.";
+            return View();
+        }
+
+        public ActionResult Doctors()
+        {
+            ViewBag.Message = "Doctors.";
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            ViewBag.Message = "Blog.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact.";
+            return View();
+        }
+
+        public ActionResult Login() {
+            ViewBag.Message = "Login.";
+            return View();
+        }
+
     }
 }
